7ce41a395c8586da8288bd60e1bf124c2cf5826b
Chris Lane <chlane@vmware.com>
2020-04-29 16:56:26 -0700

    vertical nav partially done

--- clr311-test-app/src/app/app.component.ts
+++ clr311-test-app/src/app/app.component.ts
@@ -1,26 +1,25 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnDestroy, OnInit } from '@angular/core';
 import { ClrStackBlock } from '@clr/angular';
-
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit, OnDestroy {
   title = 'clr311-test-app';
 
-    //begin modal 
-    public basic = false;
-    //end modal
+  //begin modal 
+  public basic = false;
+  //end modal
 
-    //begin stack view
-    @ViewChild('lazyBlock', { static: true })
-    lazyBlock: ClrStackBlock;
+  //begin stack view
+  @ViewChild('lazyBlock', { static: true })
+  lazyBlock: ClrStackBlock;
 
-    /*
-     * Lazy loading demo
-     */
+  //Lazy loading demo
   children: any[] = [];
 
   fetchChildren(): void {
@@ -41,35 +40,30 @@ export class AppComponent {
     resetChildren(): void {
       this.lazyBlock.expanded = false;
       this.children = [];
-
-      
     }
 
-
-    
     //Modal edit demo
-  
-  blocks: any[] = [
-    { title: 'Label 1', content: 'Content 1' },
-    {
-      title: 'Label 2',
-      content: 'Content 2',
-      children: [
-        { title: 'Sub-label 1', content: 'Sub-content 1' },
-        { title: 'Sub-label 2', content: 'Sub-content 2' },
-        { title: 'Sub-label 3', content: 'Sub-content 3' },
-      ],
-    },
-    { title: 'Label 3', content: 'Content 3' },
-  ];
+    blocks: any[] = [
+      { title: 'Label 1', content: 'Content 1' },
+      {
+        title: 'Label 2',
+        content: 'Content 2',
+        children: [
+          { title: 'Sub-label 1', content: 'Sub-content 1' },
+          { title: 'Sub-label 2', content: 'Sub-content 2' },
+          { title: 'Sub-label 3', content: 'Sub-content 3' },
+        ],
+      },
+      { title: 'Label 3', content: 'Content 3' },
+    ];
 
-  editModal = false;
+    editModal = false;
      //end stack view
 
   //overflow tabs
   inOverflow = true;
   
-//Begin Timeline 
+  //Begin Timeline 
   props = [
     {
       name: '[clrLayout]',
@@ -87,8 +81,20 @@ export class AppComponent {
       description: 'Define the symbol (icon or spinner) to use to decorate the step',
     },
   ];
-
   //End Timeline 
+
+  //Begin Vertical Nav
+
+  
+
+  //End Vertical Nav
+
+
+  ngOnInit() {} 
+
+  ngOnDestroy() {}
+  
+  
  
 
 }
--- clr311-test-app/src/app/app.module.ts
+++ clr311-test-app/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
