42b064d51b50f896804fdf47ca5b15e0eefaad3e
Chris Lane <chlane@vmware.com>
2020-04-29 19:35:25 -0700

    adding more components for sidenav groups example

--- clr311-test-app/src/app/app-routing/app-routing.module.ts
+++ clr311-test-app/src/app/app-routing/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstComponent } from '../first/first.component';
 import { SecondComponent } from '../second/second.component';
+import { ThirdComponent } from '../third/third.component';
+import { FourthComponent } from '../fourth/fourth.component';
 
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 
--- clr311-test-app/src/app/app.module.ts
+++ clr311-test-app/src/app/app.module.ts
@@ -10,11 +10,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ThirdComponent } from './third/third.component';
+import { FourthComponent } from './fourth/fourth.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ThirdComponent,
+    FourthComponent,
    
   ],
   imports: [
